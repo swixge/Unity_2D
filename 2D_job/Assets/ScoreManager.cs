080f20315d8147d6259fa930b9c4ada7d6b7be6e
Vlad <jugyrda228@gmail.com>
2022-10-30 02:43:22 +0300

    2

---
+++ 2D_job/Assets/ScoreManager.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace CompleteProject
+{
+
+    public class ScoreManager : MonoBehaviour
+    {
+
+        public static int score;
+
+        Text text;
+
+        void Awake()
+        {
+            text = GetComponent<Text>();
+            score = 0;
+        }
+
+
+        void Update()
+        {
+            text.text = "Score: " + score; 
+        }
+    }
+}
